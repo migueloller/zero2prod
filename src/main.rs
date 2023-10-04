675f4f3e37d86f0d52ae4e68c8d74299a46653b3
Miguel Oller <maoa22@gmail.com>
2023-10-03 23:07:38 -0400

    feat: add sqlx and reorganize soure code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
-use zero2prod::run;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000.");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
 
     run(listener)?.await
 }
