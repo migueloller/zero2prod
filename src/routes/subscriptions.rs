b11a24d65ff48ef9e78931a44e894082b5c9de70
Miguel Oller <maoa22@gmail.com>
2024-02-07 22:28:24 -0500

    feat: use thiserror for StoreTokenError

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -154,27 +154,13 @@ pub async fn store_token(
     Ok(())
 }
 
+#[derive(thiserror::Error)]
+#[error("A database error was encountered while trying to store a subscription token.")]
 pub struct StoreTokenError(sqlx::Error);
 
 impl std::fmt::Debug for StoreTokenError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
-    }
-}
-
-impl std::fmt::Display for StoreTokenError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "A database error was encountered while \
-            trying to store a subscription token."
-        )
-    }
-}
-
-impl std::error::Error for StoreTokenError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        Some(&self.0)
     }
 }
 
