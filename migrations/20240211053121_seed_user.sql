6cd6014771dc9a11ad84ddb2341e07e6bd4e7459
Miguel Oller <maoa22@gmail.com>
2024-02-11 00:36:00 -0500

    feat: add seed user migration

---
+++ migrations/20240211053121_seed_user.sql
@@ -0,0 +1,6 @@
+insert into users (user_id, username, password_hash)
+values (
+    '877b120c-3890-4770-b620-1f8f5b4f49fe',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$cIEnQO8qbaNI6/BEtKZK8Q$witilrF3Tlt3tmoHu1klvpKEgE4Jm9mkZV/YG9QK/EU'
+);
